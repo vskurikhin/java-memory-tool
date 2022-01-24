6b0f5632b790e62004887abe491ea25c1cf930e4
Victor N. Skurikhin <vs@lnhost.net>
2022-01-24 14:45:35 +0300

    first commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $su.svn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
